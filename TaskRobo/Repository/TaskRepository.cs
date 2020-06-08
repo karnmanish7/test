470c93af264820d5e31bd306b02f2358b4e7e1ab
github_Manish <m.karn2011@gmail.com>
2020-06-08 09:22:08 +0530

    taskscontroller and its methods added

--- TaskRobo/Repository/TaskRepository.cs
+++ TaskRobo/Repository/TaskRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using TaskRobo.Models;
 
@@ -13,26 +14,61 @@ namespace TaskRobo.Repository
         }
 
         // This method should be used to delete task details from database based upon user's email & task id
-        public int DeleteTask(string email, int taskId)
+        public int DeleteTask(string email, int? taskId)
         {
-            return 0;
+            int result = 0;
+            try
+            {
+
+                var taskToDelete = context.Categories.FirstOrDefault(x => x.CategoryID == taskId);
+                if (taskToDelete != null)
+                {
+                    context.Categories.Remove(taskToDelete);
+                    result = context.SaveChanges();
+                    return result;
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
 
         // This method should be used to get all task details from database based upon user's email
         public IReadOnlyList<UserTask> GetAllTasks(string email)
         {
+            if (context != null)
+            {
+                var getAllTasks = context.UserTasks.ToList();
+
+                return getAllTasks;
+            }
             return null;
         }
 
         // This method should be used to get task details from database based upon user's email and task id
-        public UserTask GetTaskById(string email, int taskId)
+        public UserTask GetTaskById(string email, int? taskId)
         {
+            if (context != null)
+            {
+                var result = context.UserTasks.Find(taskId);
+
+                return result;
+            }
             return null;
         }
 
         // This method should be used to save task details into database 
         public int SaveTask(UserTask task)
         {
+            if (context != null)
+            {
+                context.UserTasks.Add(task);
+                context.SaveChangesAsync();
+                return task.TaskId;
+            }
             return 0;
         }
 
