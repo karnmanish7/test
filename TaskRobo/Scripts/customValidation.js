a43aeabb2a7fce0c309a3add9c7b3f9bae8ff45d
Kapil Ghimire <kapil.ghimire@unilever.com>
2020-06-09 15:33:08 +0530

    Basic Validation Version Added

--- TaskRobo/Scripts/customValidation.js
+++ TaskRobo/Scripts/customValidation.js
@@ -1,7 +1,8 @@
 //This function should validate Task Titlte which should allow only alpha numerals
 //and should also display error message in a <p> element with id as 'titleMsg'
-function validateTitle() {
-  
+function validateTitle(title, elementId) {
+
+    $("#", elementId).html('Title should be alphanumeric');
 }
 
 //This function checks if taskContent is not empty and length is not more 50 characters
@@ -22,3 +23,39 @@ function validateStatus() {
 function validateForm() {
    
 }
+
+$(function () {
+    console.log('adding form validation');
+
+    $("form[name='task']").validate({
+        // Specify validation rules
+        rules: {
+            // The key name on the left side is the name attribute
+            // of an input field. Validation rules are defined
+            // on the right side
+            TaskTitle: "required",
+            TaskStatus: "required",
+
+            TaskContent: {
+                required: true,
+                minlength: 5
+            }
+        },
+        // Specify validation error messages
+        messages: {
+            TaskTitle: "Please enter your firstname",
+            TaskStatus: "Please enter your TaskStatus",
+            TaskContent: {
+                required: "Please provide a TaskContent",
+                minlength: "Your TaskContent must be at least 5 characters long"
+            },
+
+        },
+        // Make sure the form is submitted to the destination defined
+        // in the "action" attribute of the form when valid
+        submitHandler: function (form) {
+            form.submit();
+        }
+    });
+
+})
